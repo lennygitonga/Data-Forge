28eee627500d1416de7400a9d732326429a6ff14
Lenny Gitonga <lennygitonga07@gmail.com>
2026-04-29 13:51:39 +0300

    add Flask-Bcrypt and Flask-Login to enhance authentication; refactor app initialization for improved security

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 from app.config import Config
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+    login_manager.login_message = "Please log in to access Data-Forge."
+    login_manager.login_message_category = "warning"
 
     from app.routes import main
+    from app.auth import auth
     app.register_blueprint(main)
+    app.register_blueprint(auth)
 
     return app
